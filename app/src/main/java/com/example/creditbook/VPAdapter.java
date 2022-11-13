f4e81e8222c29f2483ec5a38f4656baa91361f1b
neimael <114741266+neimael@users.noreply.github.com>
2022-11-13 23:13:48 +0100

    add l3jeeb

---
+++ app/src/main/java/com/example/creditbook/VPAdapter.java
@@ -0,0 +1,42 @@
+package com.example.creditbook;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+
+import java.util.ArrayList;
+
+public class VPAdapter extends FragmentPagerAdapter {
+
+    private final ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
+    private final ArrayList<String> fragmentTitle = new ArrayList<>();
+
+    public VPAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentArrayList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentArrayList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragmentArrayList.add(fragment);
+        fragmentTitle.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitle.get(position);
+    }
+}
