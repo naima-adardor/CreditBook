31621e035979209c13a65d02fe2519dd7ac367d3
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2022-11-16 13:54:21 +0000

    update profile view

--- app/src/main/java/com/example/creditbook/home.java
+++ app/src/main/java/com/example/creditbook/home.java
@@ -6,23 +6,40 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class home extends AppCompatActivity implements View.OnClickListener {
-private CardView c1;
-private CardView c2;
-private CardView c3;
+private LinearLayout c1;
+private LinearLayout c2;
+private LinearLayout c3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         c1 = findViewById(R.id.profile);
+        c2 = findViewById(R.id.cashbook);
+        c3 = findViewById(R.id.creditbook);
         c1.setOnClickListener(this);
+        c2.setOnClickListener(this);
+        c3.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
-        Intent intent=new Intent(home.this,profile.class);
-        startActivity(intent);
-    }
+        switch(view.getId()){
+            case R.id.profile:
+                Intent intent1=new Intent(home.this,profile.class);
+                startActivity(intent1);
+                break;
+            case R.id.cashbook:
+                Intent intent2=new Intent(home.this,CashBookActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.creditbook:
+                Intent intent3=new Intent(home.this,client_supplier.class);
+                startActivity(intent3);
+                break;
+
+    }}
 }
