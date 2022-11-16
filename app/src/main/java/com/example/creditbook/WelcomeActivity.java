1df9b607ba0dea039d87649126cd51a4e81ff639
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2022-11-16 13:22:24 +0000

    add home in manifest

--- app/src/main/java/com/example/creditbook/WelcomeActivity.java
+++ app/src/main/java/com/example/creditbook/WelcomeActivity.java
@@ -9,8 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-public class WelcomeActivity extends AppCompatActivity {
-
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +18,14 @@ public class WelcomeActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
-
+        button = findViewById(R.id.Next_btn3);
+        button.setOnClickListener(this);
 
     }
 
+    @Override
+    public void onClick(View view) {
+        Intent intent=new Intent(WelcomeActivity.this,home.class);
+        startActivity(intent);
+    }
 }
