8c0940fee21f0a60dc0b257328b223158dc1e59c
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2022-11-16 13:34:00 +0000

    add profile to manifest

--- app/src/main/java/com/example/creditbook/home.java
+++ app/src/main/java/com/example/creditbook/home.java
@@ -1,14 +1,28 @@
 package com.example.creditbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class home extends AppCompatActivity {
-
+public class home extends AppCompatActivity implements View.OnClickListener {
+private CardView c1;
+private CardView c2;
+private CardView c3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        c1 = findViewById(R.id.profile);
+        c1.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent=new Intent(home.this,profile.class);
+        startActivity(intent);
     }
 }
