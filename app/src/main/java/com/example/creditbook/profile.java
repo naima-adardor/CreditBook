7fa6e4aa496b32a91ec682ae929f64f5d71dbf84
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2022-11-16 15:13:36 +0000

    fix bug

--- app/src/main/java/com/example/creditbook/profile.java
+++ app/src/main/java/com/example/creditbook/profile.java
@@ -1,13 +1,28 @@
 package com.example.creditbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class profile extends AppCompatActivity {
-
+public class profile extends AppCompatActivity implements View.OnClickListener  {
+    private Button home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        home = findViewById(R.id.buttonhome);
+        home.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent1=new Intent(profile.this,home.class);
+        startActivity(intent1);
+
     }
 }
