0c7635fd42b50a03ddfb7e020a11579eadcf3e9d
Maryam Bouchhar <meryem.bouchhar@gmail.com>
2022-11-20 13:00:44 +0100

    Revert "Merge pull request #18 from naima-adardor/eljid"
    This reverts commit bfae87d09a3bec75ede983aebb19794b3f2892bd, reversing
    changes made to b738250c9055be5dffb402a41bf3fb4f3b34fbfe.

--- app/src/main/java/com/example/credit__book/Activities/supplier_fragment.java
+++ app/src/main/java/com/example/credit__book/Activities/supplier_fragment.java
@@ -16,6 +16,6 @@ public class supplier_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_supplier_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_supplier, container, false);
     }
 }
