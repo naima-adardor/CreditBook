9d00480b33cdc392893a4b6db18a0cd044b3e407
Maryam Bouchhar <meryem.bouchhar@gmail.com>
2022-11-20 18:43:25 +0100

    some changes

--- app/src/main/java/com/example/credit__book/Activities/client_fragment.java
+++ app/src/main/java/com/example/credit__book/Activities/client_fragment.java
@@ -26,15 +26,8 @@ public class client_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_client, container, false);
-
-
-
-
-        return view;
+        return inflater.inflate(R.layout.fragment_client, container, false);
     }
 
     @Override
