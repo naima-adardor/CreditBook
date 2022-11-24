adaa400a7c5944f8ae8f09e266542c22180674aa
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2022-11-24 21:43:49 +0000

    View Edit C/S :)

---
+++ app/src/main/java/com/example/credit__book/Activities/EditClientSupplierActivity.java
@@ -0,0 +1,16 @@
+package com.example.credit__book.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.credit__book.R;
+
+public class EditClientSupplierActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_client_supplier);
+    }
+}
