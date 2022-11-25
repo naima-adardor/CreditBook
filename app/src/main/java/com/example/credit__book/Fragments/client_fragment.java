8dc89dc2df37615369c8511f42831bd582f94dee
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2022-11-25 18:27:37 +0000

    display

--- app/src/main/java/com/example/credit__book/Fragments/client_fragment.java
+++ app/src/main/java/com/example/credit__book/Fragments/client_fragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -21,13 +20,22 @@ import com.example.credit__book.Model.OperationClient;
 import com.example.credit__book.R;
 import com.example.credit__book.recycleview_client_interface;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class client_fragment extends Fragment  implements View.OnClickListener ,recycleview_client_interface {
 
     TextView CountOp;
     TextView countTransaction;
     OperationClientAdapter opAD;
-   FloatingActionButton ajouter;
+    FloatingActionButton ajouter;
+    DatabaseReference db;
+    ArrayList<OperationClient> client;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,21 +56,48 @@ public class client_fragment extends Fragment  implements View.OnClickListener ,
         super.onViewCreated(view, savedInstanceState);
 
         MyApplication context = (MyApplication) this.getActivity().getApplicationContext();
-        for (int i = 0; i < 10; i++) {
+     /*   for (int i = 0; i < 10; i++) {
             OperationClient listItem = new OperationClient("Naima ELJID", "20-11-2022", 500, "You have to get");
             context.getListClientOperation().add(listItem);
-        }
-        opAD = new OperationClientAdapter(context.getListClientOperation(),  this.getContext());
+        }*/
 
-        RecyclerView recyclerViewOperation = view.findViewById(R.id.recyclerViewSupplier);
+
+        RecyclerView recyclerViewOperation = view.findViewById(R.id.recyclerViewClient);
+        db= FirebaseDatabase.getInstance().getReference("OperationClients");
         recyclerViewOperation.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerViewOperation.setAdapter(opAD);
+        client =new ArrayList<>();
+        opAD = new OperationClientAdapter(client,  this.getContext());
+
         recyclerViewOperation.setHasFixedSize(true);
         CountOp = view.findViewById(R.id.textViewoperation);
         CountOp.setText("Clients(" + opAD.getItemCount() + ")");
         countTransaction = view.findViewById(R.id.textViewBalance);
         countTransaction.setText("Transactions(" + opAD.getItemCount() + ")");
         ajouter= view.findViewById(R.id.btnclient);
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                client.clear();
+                for(DataSnapshot dataSnapshot :snapshot.child("OperationClients").getChildren()){
+                    if(dataSnapshot.hasChild("operation_client_date")&&dataSnapshot.hasChild("balance_client")&&dataSnapshot.hasChild("description")&&dataSnapshot.hasChild("name_client")){
+                    final String date=dataSnapshot.child("operation_client_date").getValue(String.class);
+                    final String balance_client=dataSnapshot.child("balance_client").getValue(String.class);
+                    final String description=dataSnapshot.child("description").getValue(String.class);
+                    final String name_client=dataSnapshot.child("name_client").getValue(String.class);
+                  /*  OperationClient operationClient= dataSnapshot.getValue(OperationClient.class);
+                    client.add(operationClient);*/
+                    OperationClient operationClient=new OperationClient(name_client,date,balance_client,description);
+                    client.add(operationClient);
+                }}
+                recyclerViewOperation.setAdapter(opAD);
+                opAD.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         ajouter.setOnClickListener(this);
     }
 
